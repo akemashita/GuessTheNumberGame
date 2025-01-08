53699b97d7b24e566a2ced43e220ceb8c2c3b691
akemashita <134128275+akemashita@users.noreply.github.com>
2025-01-08 01:55:25 +0000

    add choose target integer

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import sys
+import random
 
 print('----------------------------------------')
 print('| Guess The Number Game - 数当てゲーム |')
@@ -17,6 +18,8 @@ def set_range():
 
     if len(range_values) == 2:
         print('あなたが設定した範囲は ' + str(range_values[0]) + ' 〜 ' + str(range_values[1]) + ' です。\n')
+        target = random.randint(range_values[0], range_values[1])
+        print('コンピュータが選んだ数は ' + str(target) + ' です。')
 
     else:
         print('入力が間違っています。整数を２つ入力してください。\n')
