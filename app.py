9123a0c45299c844091494787fd759f181d65f00
akemashita <134128275+akemashita@users.noreply.github.com>
2025-01-08 01:24:05 +0000

    add user range input point

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
+import sys
 
 print('----------------------------------------')
 print('| Guess The Number Game - 数当てゲーム |')
 print('----------------------------------------')
 
+userInput = input('整数を２つ入力してください（例：2, 10）：')
+print(userInput)
