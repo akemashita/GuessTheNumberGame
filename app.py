e00b1243e2f55e8595acf00775c21d8685630ca5
akemashita <134128275+akemashita@users.noreply.github.com>
2025-01-08 01:34:15 +0000

    add show user set range

--- app.py
+++ app.py
@@ -4,5 +4,14 @@ print('----------------------------------------')
 print('| Guess The Number Game - 数当てゲーム |')
 print('----------------------------------------')
 
-userInput = input('整数を２つ入力してください（例：2, 10）：')
-print(userInput)
+user_input = input('整数を２つ入力してください（例：2, 10）：')
+# print(user_input)
+
+range_values = list(map(int, user_input.split(',')))
+range_values.sort()
+
+if len(range_values) == 2:
+    print('あなたが設定した範囲は ' + str(range_values[0]) + ' 〜 ' + str(range_values[1]) + ' です。')
+
+else:
+    print('入力が間違っています。整数を２つ入力してください。')
