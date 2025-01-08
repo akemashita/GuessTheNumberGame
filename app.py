b9f42e9e2efcd03018f302e387bbd22cd419501b
akemashita <134128275+akemashita@users.noreply.github.com>
2025-01-08 01:42:02 +0000

    add set_range() and input validation

--- app.py
+++ app.py
@@ -4,14 +4,23 @@ print('----------------------------------------')
 print('| Guess The Number Game - 数当てゲーム |')
 print('----------------------------------------')
 
-user_input = input('整数を２つ入力してください（例：2, 10）：')
-# print(user_input)
+def set_range():
+    user_input = input('整数を２つ入力してください（例：2, 10）：')
 
-range_values = list(map(int, user_input.split(',')))
-range_values.sort()
+    try:
+        range_values = list(map(int, user_input.split(',')))
+        range_values.sort()
+    except ValueError:
+        print('入力が間違っています。整数を２つ入力してください。\n')
+        set_range()
+        return
 
-if len(range_values) == 2:
-    print('あなたが設定した範囲は ' + str(range_values[0]) + ' 〜 ' + str(range_values[1]) + ' です。')
+    if len(range_values) == 2:
+        print('あなたが設定した範囲は ' + str(range_values[0]) + ' 〜 ' + str(range_values[1]) + ' です。\n')
 
-else:
-    print('入力が間違っています。整数を２つ入力してください。')
+    else:
+        print('入力が間違っています。整数を２つ入力してください。\n')
+        set_range()
+
+
+set_range()
